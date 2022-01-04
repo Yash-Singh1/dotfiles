d44b74314e25dd7642c39c6203c2fcd6f475ffe1
Yash-Singh1 <saiansh2525@gmail.com>
2022-01-03 16:59:15 -0800

    chore!: remove modules and rewrite installation instructions

---
+++ install-extensions.sh
@@ -0,0 +1,1 @@
+cat .vscode/extensions | xargs -L 1 code --install-extension
--- update-git-modules.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-for module in better-git-diff nano-mermaid nano-tiprogram .oh-my-zsh .nano $(ls .vscode/extensions/ | sed 's/^/.vscode\/extensions\//'); do
-  git -C $module fetch
-done
