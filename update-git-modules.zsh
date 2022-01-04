83a07d55fdb6ae0ed01ccba690073604bee0c2b2
Yash-Singh1 <saiansh2525@gmail.com>
2022-01-03 16:14:36 -0800

    chore: add cron job for updating git modules

--- update-git-modules.zsh
+++ update-git-modules.zsh
@@ -1,7 +1,5 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 for module in better-git-diff nano-mermaid nano-tiprogram .oh-my-zsh .nano $(ls .vscode/extensions/ | sed 's/^/.vscode\/extensions\//'); do
-  cd ~/dotfiles/$module
-  git pull
-  cd ~/dotfiles
+  git -C $module fetch
 done
